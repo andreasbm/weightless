3a2d82cdf25d087aba3f42c9d78328c712e2923d
andreasbm <andmehlsen@gmail.com>
2019-02-28 21:00:07 +0100

    Added iframe karma framework.

--- karma.conf.js
+++ karma.conf.js
@@ -3,9 +3,22 @@ const {defaultResolvePlugins, defaultKarmaConfig} = require("@appnest/web-config
 module.exports = (config) => {
 	config.set({
 		...defaultKarmaConfig({
-			rollupPlugins: defaultResolvePlugins()
+			rollupPlugins: defaultResolvePlugins(),
+			karmaPlugins: [
+				"karma-iframes"
+			]
 		}),
 		basePath: "src",
-		logLevel: config.LOG_INFO
+		logLevel: config.LOG_INFO,
+		preprocessors: {
+			"**/*.test.+(ts|js)": ["rollup", "iframes"]
+		},
+		frameworks: ["mocha", "chai", "iframes"],
+		client: {
+			captureConsole: true,
+			mocha: {
+				bail: true
+			}
+		}
 	});
 };
