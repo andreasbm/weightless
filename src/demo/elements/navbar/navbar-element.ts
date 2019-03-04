ce1326b788a31996fb20809c356746544cd1613e
andreasbm <andmehlsen@gmail.com>
2019-03-04 19:09:53 +0100

    Added octocat.

--- src/demo/elements/navbar/navbar-element.ts
+++ src/demo/elements/navbar/navbar-element.ts
@@ -11,6 +11,7 @@ import { cssResult } from "../../../lib/util/css";
 import { setProperty } from "../../../lib/util/dom";
 import { Palette, setPalette } from "../../../lib/util/theme";
 import { sharedStyles } from "../../style/shared";
+import "../octo-banner/octo-element";
 import "../theme/theme-element";
 import styles from "./navbar-element.scss";
 
@@ -94,7 +95,14 @@ export class ThemeComponent extends LitElement {
 						<icon-element>menu</icon-element>
 					</button-element>
 					<router-link id="logo-wrapper" path="/" @click="${() => this.rotateLogo()}">
-						<img id="logo" src="assets/logo.svg" />
+						<svg id="logo" width="100%" height="100%" viewBox="0 0 264 264" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+							<g fill-rule="nonzero">
+								<path d="M217.5 132h-20v-22.5c0-49.43-40.07-89.5-89.5-89.5-5.523 0-10-4.477-10-10s4.477-10 10-10c60.475 0 109.5 49.025 109.5 109.5V132z"/>
+								<path d="M132 46.5v20h-22.5C60.07 66.5 20 106.57 20 156c0 5.523-4.477 10-10 10s-10-4.477-10-10C0 95.525 49.025 46.5 109.5 46.5H132z"/>
+								<path d="M46.5 132h20v22.5c0 49.43 40.07 89.5 89.5 89.5 5.523 0 10 4.477 10 10s-4.477 10-10 10c-60.475 0-109.5-49.025-109.5-109.5V132z"/>
+								<path d="M132 217.5v-20h22.5c49.43 0 89.5-40.07 89.5-89.5 0-5.523 4.477-10 10-10s10 4.477 10 10c0 60.475-49.025 109.5-109.5 109.5H132z"/>
+							</g>
+						</svg>
 						<img id="text" src="${this.darkMode ? `assets/text-light.svg` : `assets/text-dark.svg`}" />
 					</router-link>
 				</div>
@@ -112,6 +120,7 @@ export class ThemeComponent extends LitElement {
 							<theme-element @update="${() => this.$themePopover.hide()}"></theme-element>
 						</popover-card-element>
 					</popover-element>
+					<a id="octo" href="https://github.com/andreasbm/weightless" target="_blank"><octo-element></octo-element></a>
 				</div>
 			</nav-element>
 		`;
