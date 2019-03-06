298ce4ba9acf2986e1c6dc83ba40bc5bb0eb3656
andreasbm <andmehlsen@gmail.com>
2019-03-06 14:39:31 +0100

    Bumped deps.

--- karma.conf.js
+++ karma.conf.js
@@ -4,17 +4,10 @@ const path = require("path");
 module.exports = (config) => {
 	config.set({
 		...defaultKarmaConfig({
-			rollupPlugins: defaultResolvePlugins(),
-			karmaPlugins: [
-				"karma-iframes"
-			]
+			rollupPlugins: defaultResolvePlugins()
 		}),
 		basePath: "src",
 		logLevel: config.LOG_INFO,
-		preprocessors: {
-			"**/*.test.+(ts|js)": ["rollup", "iframes"]
-		},
-		frameworks: ["mocha", "chai", "iframes"],
 		client: {
 			captureConsole: true,
 			mocha: {
